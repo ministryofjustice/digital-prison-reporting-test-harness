da7d6a85acc84e96a6736de228781378a2c95eca
Hari Shanmugam <hari.shanmugam@digital.justice.gov.uk>
2023-03-28 15:26:32 +0100

    Updated test data

--- tests/load_data_to_stream.py
+++ tests/load_data_to_stream.py
@@ -1,24 +1,26 @@
-import sys,os
-sys.path.append(os.pardir)
+import json
+import base64
+from utils.config import getConfig
 from utils.kinesis import KinesisStream
-import base64,json,os
+import sys
+import os
+sys.path.append(os.pardir)
 
 # GIVEN: ARRANGE
 
-stream = KinesisStream("dpr-297-datastream3")
-profileName = "hari_profile"
-    
+profileName = getConfig("aws", "profile")
+stream = KinesisStream(getConfig("kinesis", "stream"))
+
 # STUB TABLE DATA
 
 tableName = "OFFENDER_BOOKINGS"
-# payloadFile = "agency_locations_happy_path.json"
+# payloadFile = "agency_locations_happy_path.json"c
 # payloadFile = "offenders_happy_path.json"
 # payloadFile = "agency_internal_locations_happy_path.json"
 
 
-payloadRepo_happy_path = '/Users/hari.shanmugam/ministryofjustice/dpr_test_harness/payloads/happy_paths'
-
-payloadRepo_error_scenarios = '/Users/hari.shanmugam/ministryofjustice/dpr_test_harness/payloads/error_scenarios'
+payloadRepo_happy_path = os.pardir+'/payloads/happy_scenarios'
+payloadRepo_error_scenarios = os.pardir+'payloads/error_scenarios'
 
 
 for filename in os.listdir(payloadRepo_happy_path):
@@ -27,12 +29,13 @@ for filename in os.listdir(payloadRepo_happy_path):
             payload = json.dumps(json.load(f))
             base64data = base64.b64encode(payload.encode())
 
-print("\n"+"Data source {} : Table Name {}".format("NOMIS",tableName)+"\n")
+print("\n"+"Data source {} : Table Name {}".format("NOMIS", tableName)+"\n")
 
 # WHEN:POST DATA TO KINESIS STREAM : ACT
 
 for i in range(20):
     response = stream.send_stream(data=payload, profileName=profileName)
-    print("Record ID -- {}  ShardId -- {}  SequenceNumber -- {}  HTTPStatusCode -- {}".format(i+1,response["ShardId"],response["SequenceNumber"],response["ResponseMetadata"]["HTTPStatusCode"]))
+    print("Record ID -- {}  ShardId -- {}  SequenceNumber -- {}  HTTPStatusCode -- {}".format(i+1,
+          response["ShardId"], response["SequenceNumber"], response["ResponseMetadata"]["HTTPStatusCode"]))
 
 # THEN: Deltalake created in S3 RAW ZONE :ASSERT
